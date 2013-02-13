c2ba390029225f66579f2d3fbdee9050c3611395
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2013-02-13 23:24:42 +0200

    Get correct numbers for ex1.2 and use correct rational literal.

--- ex1.2.rkt
+++ ex1.2.rkt
@@ -3,6 +3,9 @@
 ;; about second summond in numerator
 ;; Hope it is '1'
 
-(/ (+ 5 1 (- 2 (- 3 (+ 6 (/ 1 3)))))
-   (* 3 (- 6 2) (- 2 7))
-   )
+;; UPD: get correct numbers
+
+;; Result --> -37/150
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
+   (* 3 (- 6 2) (- 2 7)))
