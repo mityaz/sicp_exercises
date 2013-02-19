83da19c68e3d773e8d234a25646a972dba96f035
Dmitriy Ugnichenko <dmitriy.ugnichenko@anahoret.com>
2013-02-19 17:11:20 +0200

    Correct version of good-enough? (now really using fraction part).

--- ex1.7.rkt
+++ ex1.7.rkt
@@ -2,79 +2,20 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (_sqrt x)
-  (define (sqrt-iter guess x)
-    (define (good-enough? guess)
-      (< (abs (- (square guess) x)) 0.001))
-    (define (improve guess)
-      (average guess (/ x guess)))
-    (if (good-enough? guess)
-        guess
-        (sqrt-iter (improve guess)
-                   x)))
-  (sqrt-iter 1.0 x))
-
 ;; Improved sqrt-proc, with modified good-enough? proc
 
-(define (__sqrt x)
+(define (sqrt% x)
   (define (next-step guess next-guess)
     (if (good-enough? guess next-guess)
-        guess
+        next-guess
         (sqrt-iter next-guess x)))
   (define (good-enough? guess next-guess)
-    (< (abs (- guess next-guess)) 0.001))
+    (< (abs (/ (- guess next-guess) next-guess)) 0.001))
   (define (improve guess)
     (average guess (/ x guess)))
   (define (sqrt-iter guess x)
     (next-step guess (improve guess)))
   (sqrt-iter 1.0 x))
 
-;;
-(__sqrt 36)
-;;
-
-;; Tests to show, that current implemenation
-;; is not valid for large and small numbers
-
-(square (_sqrt 12345))
-(square (_sqrt 123456))
-(square (_sqrt 1234567))
-(square (_sqrt 12345678))
-(square (_sqrt 123456789))
-(square (_sqrt 1234567890))
-(square (_sqrt 12345678901))
-(square (_sqrt 123456789012))
-(square (_sqrt 1234567890123))
-
-(square (_sqrt 0.123))
-(square (_sqrt 0.00123))
-(square (_sqrt 0.000123))
-(square (_sqrt 0.0000123))
-(square (_sqrt 0.00000123))
-(square (_sqrt 0.000000123))
-(square (_sqrt 0.0000000123))
-(square (_sqrt 0.00000000123))
-(square (_sqrt 0.000000000123))
-
-;; Same tests with alternative implementation
-;; for good-enough?
-
-(square (__sqrt 12345))
-(square (__sqrt 123456))
-(square (__sqrt 1234567))
-(square (__sqrt 12345678))
-(square (__sqrt 123456789))
-(square (__sqrt 1234567890))
-(square (__sqrt 12345678901))
-(square (__sqrt 123456789012))
-(square (__sqrt 1234567890123))
-
-(square (__sqrt 0.123))
-(square (__sqrt 0.00123))
-(square (__sqrt 0.000123))
-(square (__sqrt 0.0000123))
-(square (__sqrt 0.00000123))
-(square (__sqrt 0.000000123))
-(square (__sqrt 0.0000000123))
-(square (__sqrt 0.00000000123))
-(square (__sqrt 0.000000000123))
+;; Test sample
+(square (sqrt% 1e-12)) ;; yuppi!!!! now it's really 1e-12 again!!!
--- ex1.8.rkt
+++ ex1.8.rkt
@@ -1,31 +1,17 @@
 
 (define (cubic x) (* x x x))
 
-(define (cubrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (cubrt-iter (approximate guess x)
-                  x)))
-
-(define (approximate guess x)
-  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
-
-(define (good-enough? guess x)
-  (< (abs (- (cubic guess) x)) 0.001))
-
-(define (cubrt x) (cubrt-iter 1.0 x))
-
 ;;
 ;; Improved version
 ;;
 
-(define (_cubrt x)
+(define (cubrt x)
   (define (next-step guess next-guess)
     (if (good-enough? guess next-guess)
         guess
         (cubrt-iter next-guess x)))
   (define (good-enough? guess next-guess)
-    (< (abs (- guess next-guess)) 0.001))
+    (< (abs (/ (- guess next-guess) next-guess)) 0.001))
   (define (approximate guess)
     (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
   (define (cubrt-iter guess x)
@@ -36,12 +22,4 @@
 (cubic(cubrt 27))
 (cubic(cubrt 1000))
 (cubic(cubrt 0.002))
-
-(cubic(_cubrt 27))
-(cubic(_cubrt 1000))
-(cubic(_cubrt 0.002))
-
-(cubrt (cubic 0.002))   ; answer is far from truth
-(_cubrt (cubic 0.002))  ; pretty close
-
-
+(cubic(cubrt 1e-12))
